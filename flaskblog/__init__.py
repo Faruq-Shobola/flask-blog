e6e1e2bcfdcdfce7b16fd8b32548e1506dbf89d3
shobola faruq <shobolafaruq@gmail.com>
2022-09-16 11:41:41 +0100

    Add user registration feature

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '49ddf468eb7a3d5ab597ccfd87ec41a4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
