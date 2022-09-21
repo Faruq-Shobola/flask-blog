59636e6454da546a5640bd7e0657c57494677010
shobola faruq <shobolafaruq@gmail.com>
2022-09-21 18:34:59 +0100

    Multiple registration fixed, Login feature added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '49ddf468eb7a3d5ab597ccfd87ec41a4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
