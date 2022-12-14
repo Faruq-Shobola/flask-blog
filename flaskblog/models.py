1fcc259a9418292e3e547b4ee76255c55a8af27b
shobola faruq <shobolafaruq@gmail.com>
2022-12-14 20:43:23 +0100

    Set variable to env variable

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,9 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from itsdangerous.encoding import base64_encode as encode
+from flaskblog import db, login_manager, app # jws
 from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,6 +17,26 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    # def get_reset_token(self):
+    #     protected = {'alg': 'HS256'}
+    #     payload = {'user_id': self.id}.decode('utf-8')
+    #     secret = app.config['SECRET_KEY']
+    #     token = jws.serialize_compact(protected, payload, secret)
+    #     return token
+
+    def get_reset_token(self):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id}).encode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
