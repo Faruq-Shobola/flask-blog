5680436fca90aabd5c0775e54eb165bccd0cc304
shobola faruq <shobolafaruq@gmail.com>
2022-12-14 19:13:19 +0100

    Implement forget password feature

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,15 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '2ae02d00ab194f59413497b49493e79c'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-bcrypt = Bcrypt()
+bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'hovernighttea@gmail.com'
+app.config['MAIL_PASSWORD'] = 'fjxpjacknjlrdhkc'
+mail = Mail(app)
 
 from flaskblog import routes
